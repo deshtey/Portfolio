f87cedf1e64c704772535478dbc692e4f4ab771f
deshtey <25150463+deshtey@users.noreply.github.com>
2019-05-07 17:56:11 +0300

    Worked on the menu Button JavaScript and rotation

---
+++ main.js
@@ -0,0 +1,32 @@
+//Cache the DOM
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuAvatar = document.querySelector(".menu-avatar");
+const navMenu = document.querySelector(".nav-menu");
+const navItems = document.querySelectorAll(".nav-item");
+
+//Menu should be set to show by default
+let showMenu = false;
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    navMenu.classList.add("show");
+    menuAvatar.classList.add("show");
+    navItems.forEach(item => {
+      item.classList.add("show");
+      showMenu = true;
+    });
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    navMenu.classList.remove("show");
+    menuAvatar.classList.remove("show");
+    navItems.forEach(item => {
+      item.classList.remove("show");
+      showMenu = false;
+    });
+  }
+}
